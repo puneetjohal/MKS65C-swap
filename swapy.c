5c0b11b4448a8f2dcf89a2c3b0966bb9209512b9
Puneet <pjohal@stuy.edu>
2018-09-20 03:49:39 -0400

    first part

---
+++ swapy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  //Create an array large enough to store 10 ints.
+  int ray[10];
+  //Populate the array with random values.
+  srand( time(NULL) );
+  int i;
+  for (i = 0; i < 9; i++){
+    *ray[i] = rand();
+  }
+  //Set the last value in the array to 0.
+  *ray[9] = 0;
+  //Print out the values in this array
+  printf("Vales in array: ");
+  for (i = 0; i < 10; i++){
+    printf("%d, ", *ray[i]);
+  }
+  return 0;
+}
